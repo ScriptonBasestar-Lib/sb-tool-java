c230d70cb5123eb1a0804ddd24dc4ba599e40c9c
chaeeung.e <chaeeung.e@solarconnect.kr>
2017-10-24 19:56:12 +0900

    security renewal

--- spring/security/src/main/java/org/scriptonbasestar/spring/security/social/SBSocialAuthenticationManager.java
+++ spring/security/src/main/java/org/scriptonbasestar/spring/security/social/SBSocialAuthenticationManager.java
@@ -1,8 +1,18 @@
 package org.scriptonbasestar.spring.security.social;
 
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+
 /**
  * @author chaeeung.e
  * @since 2017-10-24
  */
-public class SBSocialAuthenticationManager implements Authenti{
+public class SBSocialAuthenticationManager implements AuthenticationManager {
+
+	@Override
+	public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+		return null;
+	}
+
 }
